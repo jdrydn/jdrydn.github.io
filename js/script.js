4a24a3c2a4dc1c997fb9080a73472352cdfd0ef8
jdrydn <james@jdrydn.com>
2020-03-16 17:31:35 +0000

    New website design

--- js/script.js
+++ js/script.js
@@ -1,13 +1,35 @@
 (function () {
   window.loadQueue = window.loadQueue || [];
 
-  function init() {
-    for (var i = 0; i < window.loadQueue.length; i++) {
-      if (typeof window.loadQueue[i] === 'function') window.loadQueue[i].call();
+  function eachElement(els, eachFn) {
+    if (els.length) {
+      for(var i = 0; i < els.length; i++) {
+        eachFn(els[i], i, els);
+      }
     }
-	  window.loadQueue.push = function(c) { c(); };
   }
 
-  if (document.readyState !== 'loading') init();
-  else document.addEventListener('DOMContentLoaded', init);
+  window.loadQueue.push(function externalLinks() {
+    eachElement(document.getElementsByTagName('a'), function (el) {
+      if (el.getAttribute('href') && el.hostname !== window.location.hostname) {
+        el.target = '_blank';
+        // console.log('Settings', el);
+      } else {
+        // console.log('Skipping', el);
+      }
+    });
+  });
+
+  function triggerLoadQueue() {
+    // If any new loadQueue calls hit the array, automatically execute them
+    window.loadQueue.push = function(c) { c(); };
+
+    // Run through each function in the loadQueue and execute them
+    (Array.isArray(window.loadQueue) ? window.loadQueue : [])
+      .filter(function (fn) { return typeof fn === 'function' })
+      .forEach(function (fn) { fn(); });
+  }
+
+  if (document.readyState !== 'loading') triggerLoadQueue();
+  else document.addEventListener('DOMContentLoaded', triggerLoadQueue);
 })();
